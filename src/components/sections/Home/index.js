10b08595d691d8acc5536fbadd97b672524a30f9
George Ciubotaru <george.ciubotaru@emphaticspark.com>
2017-09-20 19:20:24 +0300

    feat: implement request for fetch news via API

--- src/components/sections/Home/index.js
+++ src/components/sections/Home/index.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
+import fetch from 'isomorphic-fetch';
 
 export default class Home extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+			news: []
+		};
+		this.fetchNews = this.fetchNews.bind(this);
+	}
+	
+	componentDidMount() {
+		this.fetchNews();
+	}
+	
+	fetchNews = () => {
+		return fetch(`https://pitchday.io/api/announcements`)
+			.then(response => response.json())
+			.then(json => this.setState({news: json.data[0]}))
+			.catch(er => {
+				console.log(er);
+			});
+	};
+	
 	render() {
 		return (
 			<section className="section home-section">
 				<div className="fresh-news-badge">
 					<span className="badge">News</span>
-					<span className="news-title">Here you will be able to see latest
-						breaking news about Pitchday! Stay tuned for more…</span>
+					<span className="news-title">
+						{this.state.news.Body}
+					</span>
 				</div>
 				<div className="jumbotron-block">
 					<h2 className="jumbotron-heading">Collaborative creation reborns</h2>
