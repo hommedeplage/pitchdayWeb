3b98902f077ce24432baeb469cc7d1405693692b
George Ciubotaru <george.ciubotaru@emphaticspark.com>
2017-09-28 19:52:58 +0300

    fix: hide menu items

--- src/components/Nav/MenuList.js
+++ src/components/Nav/MenuList.js
@@ -3,59 +3,68 @@ import * as utils from '../../utils/';
 import LanguageSelector from './LanguageSelector';
 
 export default class MenuList extends Component {
-	
-	constructor(props) {
-		super(props);
-		this.handleClick = this.handleClick.bind(this);
-	}
-	
-	handleClick = (id) => {
-		utils.smoothScroll.scrollTo(id);
-	};
-	
-	render() {
-		return (
-			<ul className="main-nav align-right inline">
-				<li>
-					<a onClick={() => {
-						this.handleClick('how-it-works')
-					}}>
-						How It works</a>
-				</li>
-				<li>
-					<a href="https://github.com/pitchday/white-paper"
-						target="_blank"
-                       rel="noopener noreferrer">
-						WhitePaper</a>
-				</li>
-				<li>
-					<a onClick={() => {
-						this.handleClick('allocation')
-					}}>
-						Allocation</a>
-				</li>
-				<li>
-					<a onClick={() => {
-						this.handleClick('community')
-					}}>
-						Team & Community</a>
-				</li>
-				<li>
-					<a onClick={() => {
-						this.handleClick('contact')
-					}}>
-						Get in Touch</a>
-				</li>
-				<li className="button-item">
-					<a href="https://t.me/pitchday_bot" rel="noopener noreferrer" target="_blank"
-					   className="jumbotron-button">
-						<span>JOIN NOW</span>
-					</a>
-				</li>
-				<li className="selector-item">
-					<LanguageSelector/>
-				</li>
-			</ul>
-		);
-	}
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick = id => {
+        utils.smoothScroll.scrollTo(id);
+    };
+
+    render() {
+        return (
+            <ul className="main-nav align-right inline">
+                {/*<li>*/}
+                {/*<a onClick={() => {*/}
+                {/*this.handleClick('how-it-works')*/}
+                {/*}}>*/}
+                {/*How It works</a>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*<a href="https://github.com/pitchday/white-paper"*/}
+                {/*target="_blank"*/}
+                {/*rel="noopener noreferrer">*/}
+                {/*WhitePaper</a>*/}
+                {/*</li>*/}
+                {/*<li>*/}
+                {/*<a onClick={() => {*/}
+                {/*this.handleClick('allocation')*/}
+                {/*}}>*/}
+                {/*Allocation</a>*/}
+                {/*</li>*/}
+                <li>
+                    <a
+                        onClick={() => {
+                            this.handleClick('community');
+                        }}
+                    >
+                        Pitchday Network
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="https://t.me/gpellizzari"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Get in Touch
+                    </a>
+                </li>
+                <li className="button-item">
+                    <a
+                        href="https://t.me/pitchday_bot"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="jumbotron-button"
+                    >
+                        <span>Join Network</span>
+                    </a>
+                </li>
+                <li className="selector-item">
+                    <LanguageSelector />
+                </li>
+            </ul>
+        );
+    }
 }
